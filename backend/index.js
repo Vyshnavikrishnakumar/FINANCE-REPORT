c4cb03224f74b42045e0750403952693a9f051fa
gouthamsanthosh-gsx <gouthamsanthosh.xyz@gmail.com>
2024-07-28 13:58:09 +0530

    Fixed signup with empty values

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,20 @@
 var express = require("express");
 require("./connection");
 var { UserModel, AdminModel } = require("./model");
-const jwt = require('jsonwebtoken');
-const cookiesMiddleware = require('universal-cookie-express');
+const jwt = require("jsonwebtoken");
+const cookiesMiddleware = require("universal-cookie-express");
 const cors = require('cors');
+
+const tokenSecret = "hd8weh38dsh8sdJJ9asn==";
 
 var app = express();
 app.use(express.json());
 app.use(cookiesMiddleware());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true })); // Add CORS middleware
+app.use(cors({ origin: "http://localhost:5173", credentials: true })); // Add CORS middleware
 
 // api
 app.use( ( req, res, next ) => {
-		req.session = req.universalCookies.get('session')
+		req.session = req.universalCookies.get("session")
 		next()
 })
 
@@ -23,11 +25,15 @@ app.post("/api/verify",async(req,res)=>{
 
 app.post("/api/signup",async(req,res)=>{
 	try {
-		const { username } = req.body;
+		const { username,password } = req.body;
 		const userData = await UserModel.findOne({ username });
-		if (userData && userData.username === username) {
-			res.status(401).send('User already exists!');
-		} else {
+		if (username.trim() === "" || password.trim() === ""){
+			res.status(401).send("Please fill all the field(s)!");
+		}
+		else if(userData && userData.username === username) {
+			res.status(401).send("User already exists!");
+		}
+		else {
 			await UserModel(req.body).save();
 			res.send("User Created");
 		}
@@ -41,7 +47,7 @@ app.post("/api/signin", async (req, res) => {
 	const { username, password } = req.body;
 	const userData = await UserModel.findOne({ username });
 	if (userData && userData.password === password) {
-		const token = jwt.sign({ id: userData._id }, 'hd8weh38dsh8sdJJ9asn==');
+		const token = jwt.sign({ id: userData._id }, tokenSecret);
 		res.send({ token });
 	} else {
 		res.status(401).send('Invalid login credentials');
@@ -53,7 +59,7 @@ app.post("/api/admin/signin", async (req, res) => {
 	const { username, password } = req.body;
 	const adminData = await AdminModel.findOne({ username });
 	if (adminData && adminData.password === password) {
-		const token = jwt.sign({ id: adminData._id }, 'hd8weh38dsh8sdJJ9asn==');
+		const token = jwt.sign({ id: adminData._id }, tokenSecret);
 		res.send({ token });
 	} else {
 		res.status(401).send('Invalid login credentials');
